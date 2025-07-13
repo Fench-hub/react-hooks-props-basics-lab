c0b5ccc27f12fffe9502c2b13910637898f8c54b
felona jepchumba <felonajepchumba@gmail.com>
2025-07-13 14:00:00 +0300

    Fix Links component and pass props correctly

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,19 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+import user from "../data/user"; 
 
 function App() {
+  const { name, color, city, bio, links } = user;
+
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={name} city={city} color={color} />
+      <About bio={bio} github={links.github} linkedin={links.linkedin} />
     </div>
   );
 }
 
 export default App;
+
